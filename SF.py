55073a9fb7e5af57f959ac83b1b8ec162c8f4951
sucbin.cho <subin.cho.52@gmail.com>
2018-11-06 20:09:05 +0900

    Update SF.py

--- SF.py
+++ SF.py
@@ -56,7 +56,7 @@ for sample in sample_list:
 #######################################################################################
 #STAD fasta gz file has sample_id
 
-sample_list=['5f515897-5e5e-4ced-bb81-ea0ae801cf29']
+sample_list=['ffe3c7c3-b36a-4e47-9d98-0e9ffbdc41a8']
 for sample in sample_list:
     lninf=glob.glob("/storage2/Project/TCGA_fusion/Raw_TCGA/%s/fasta/%s/*.tar"%(cancer,sample))
     #['/storage2/Project/TCGA_fusion/Raw_TCGA/STAD/fasta/00a4e56f-c9f4-4c3d-b795-9b468de83223/TCGA-D7-6818-01A-11R-1884-13_rnaseq_fastq.tar']
